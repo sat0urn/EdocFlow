994c8cc739b6601078685ae9c04163ca1dabc010
Aslan <nissanfordo03@gmail.com>
2024-04-23 19:06:55 +0500

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,13 +10,13 @@ function Footer() {
       }>
       <div className="container">
         <div className="row small opacity-75">
-          <div className="col-md-6">
+          <div className="col-md-8">
             ©2024 EDMS. Development and support of corporate software. All rights reserved.
           </div>
-          <div className="col-md-3 text-end">
+          <div className="col-md-2 text-end">
             Astana, Kazakhstan
           </div>
-          <div className="col-md-3 text-end">
+          <div className="col-md-2 text-end">
             +7 775 445 75 45
           </div>
         </div>
