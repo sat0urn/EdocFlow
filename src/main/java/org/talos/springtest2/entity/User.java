476d36aa2f91905697cf64c2a924a708edbbdff2
Zeiin1 <zeyin03@gmail.com>
2024-04-27 15:42:47 +0600

    added additional attribute inside user class List of Documents

--- src/main/java/org/talos/springtest2/entity/User.java
+++ src/main/java/org/talos/springtest2/entity/User.java
@@ -4,7 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
 
 @Document(collection = "users")
 @Data
@@ -20,4 +23,6 @@ public class User {
     private String password;
     private String country;
     private String city;
+  //  @DBRef uncommit if one document will belong to different users
+    private List<PDFDocument> documents;
 }
