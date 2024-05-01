9d91b4c270271a396a0996f3acd3b42373e864d0
Aslan <nissanfordo03@gmail.com>
2024-05-01 21:56:51 +0500

    index_url

--- src/http/index.js
+++ src/http/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const $authHost = axios.create({
-    baseURL: 'http://localhost:8080/'
+    baseURL: 'https://edoc-flow-api.onrender.com/'
 })
 
 const $host = axios.create({
-    baseURL: 'http://localhost:8080/'
+    baseURL: 'https://edoc-flow-api.onrender.com/'
 })
 
 const authInterceptor = config => {
