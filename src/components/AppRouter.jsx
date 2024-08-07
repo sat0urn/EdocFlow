dccf63b7b8d632aa8895509688f1c87dfa25429b
Aslan <nissanfordo03@gmail.com>
2024-08-07 23:00:22 +0500

    gh-pages deploy check

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,24 +1,16 @@
-import {Route, Routes} from 'react-router-dom'
-import {observer} from "mobx-react-lite";
-import Layout from "./Layout.jsx";
-import Main from "../pages/Main.jsx";
-import SignIn from "../pages/SignIn.jsx";
+import {Routes} from 'react-router-dom'
+import RenderRoutes from "./RenderRoutes.jsx";
+import {useContext} from "react";
+import {AuthContext} from "../context/index.js";
 
-const AppRouter = observer(() => {
-  // const {user} = useContext(AuthContext)
+const AppRouter = () => {
+  const {user} = useContext(AuthContext)
 
   return (
     <Routes>
-      {/*{RenderRoutes({user})}*/}
-      <Route path={'/'} element={<Layout/>}>
-        <Route index element={<Main/>}/>
-        <Route path={'login'} element={<SignIn/>}/>
-        {/*{publicRoutes.map(({id, path, element}) =>*/}
-        {/*  <Route key={id} path={path} element={element}/>*/}
-        {/*)}*/}
-      </Route>
+      {RenderRoutes({user})}
     </Routes>
   )
-})
+}
 
 export default AppRouter
