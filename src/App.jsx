f3888531b999b80fc1edd98d26782209e4129a50
Aslan <nissanfordo03@gmail.com>
2024-08-08 13:03:04 +0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,13 @@ import {BrowserRouter} from 'react-router-dom'
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import AppRouter from './components/AppRouter'
-import {AuthContext} from './context/index'
-import {useContext} from 'react'
 
 const App = () => {
-  const {user} = useContext(AuthContext)
-  // const [isLoading, setIsLoading] = useState(true)
-
-  // useEffect(() => {
-  //   check()
-  //     .then(userData => {
-  //       console.log('user')
-  //       user.setUser(userData)
-  //       user.setRole(userData.role)
-  //       user.setIsAuth(true)
-  //     })
-  //     .catch(() => {
-  //     })
-  //     .finally(() => setIsLoading(false))
-  // }, [])
-  //
-  // if (isLoading) {
-  //   return <Loader/>
-  // }
-
   return (
     <BrowserRouter basename={"/edoc_flow"}>
-      {!user.isAuth && <Header/>}
+      <Header/>
       <AppRouter/>
-      {!user.isAuth && <Footer/>}
+      <Footer/>
     </BrowserRouter>
   )
 }
--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,25 +1,13 @@
 import {Navigate, Route, Routes} from 'react-router-dom'
-import {useContext} from "react";
-import {AuthContext} from "../context/index.js";
 import Layout from "./Layout.jsx";
 import Main from "../pages/Main.jsx";
-import SignIn from "../pages/SignIn.jsx";
-import SignUp from "../pages/SignUp.jsx";
-import RegisterBusiness from "../pages/RegisterBusiness.jsx";
 import Recover from "../pages/Recover.jsx";
 
 const AppRouter = () => {
-  const {user} = useContext(AuthContext)
-
-  console.log(user)
-
   return (
     <Routes>
       <Route path={'/'} element={<Layout/>}>
         <Route index element={<Main/>}/>
-        <Route path={'login'} element={<SignIn/>}/>
-        <Route path={'register'} element={<SignUp/>}/>
-        <Route path={'registerBusiness'} element={<RegisterBusiness/>}/>
         <Route path={'recover'} element={<Recover/>}/>
         <Route path={'*'} element={<Navigate to={'/'}/>}/>
       </Route>
