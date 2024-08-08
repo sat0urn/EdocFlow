9b83a704130051982c8be77f7aaf325bf58b57d5
Aslan <nissanfordo03@gmail.com>
2024-08-08 13:00:03 +0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,28 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import AppRouter from './components/AppRouter'
 import {AuthContext} from './context/index'
-import {useContext, useEffect, useState} from 'react'
-import Loader from './components/Loader'
-import {observer} from "mobx-react-lite";
-import {check} from "./http/userApi.js"
+import {useContext} from 'react'
 
-const App = observer(() => {
+const App = () => {
   const {user} = useContext(AuthContext)
-  const [isLoading, setIsLoading] = useState(true)
+  // const [isLoading, setIsLoading] = useState(true)
 
-  useEffect(() => {
-    check()
-      .then(userData => {
-        console.log('user')
-        user.setUser(userData)
-        user.setRole(userData.role)
-        user.setIsAuth(true)
-      })
-      .catch(() => {
-      })
-      .finally(() => setIsLoading(false))
-  }, [])
-
-  if (isLoading) {
-    return <Loader/>
-  }
+  // useEffect(() => {
+  //   check()
+  //     .then(userData => {
+  //       console.log('user')
+  //       user.setUser(userData)
+  //       user.setRole(userData.role)
+  //       user.setIsAuth(true)
+  //     })
+  //     .catch(() => {
+  //     })
+  //     .finally(() => setIsLoading(false))
+  // }, [])
+  //
+  // if (isLoading) {
+  //   return <Loader/>
+  // }
 
   return (
     <BrowserRouter basename={"/edoc_flow"}>
@@ -36,6 +33,6 @@ const App = observer(() => {
       {!user.isAuth && <Footer/>}
     </BrowserRouter>
   )
-})
+}
 
 export default App
