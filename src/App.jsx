750c02bf3087f6d53fd97843161d69fb8e7ff816
Aslan <nissanfordo03@gmail.com>
2024-08-09 13:46:11 +0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import {BrowserRouter} from 'react-router-dom'
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import AppRouter from './components/AppRouter'
+import Main from "./components/pages/Main.jsx";
 
 const App = () => {
   return (
     <BrowserRouter basename={"/edoc_flow"}>
       <Header/>
-      <AppRouter/>
+      <Main/>
       <Footer/>
     </BrowserRouter>
   )
