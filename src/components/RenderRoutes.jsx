68658ca5fa9e5dd1ac051fb6ea7241b2f33f8d78
Aslan <nissanfordo03@gmail.com>
2024-08-09 15:00:15 +0500

    test

--- src/components/RenderRoutes.jsx
+++ src/components/RenderRoutes.jsx
@@ -8,16 +8,15 @@ import {
 } from "../router/routes.jsx";
 import Profile from "../pages/Profile.jsx";
 import {ADMIN, EMPLOYEE, INDEPENDENT_USER, OFFICE_MANAGER} from "../data/userRolesData.js";
-import Layout from "./Layout.jsx";
 
 const RenderRoutes = ({user}) => {
   if (!user.isAuth) {
     return (
-      <Route element={<Layout/>}>
+      <>
         {publicRoutes.map(({id, path, element}) =>
           <Route key={id} path={path} element={element}/>
         )}
-      </Route>
+      </>
     )
   }
 
