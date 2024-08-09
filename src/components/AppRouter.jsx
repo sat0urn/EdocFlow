ad194ce5319b71f6c89209dea669f13d7f7ed488
Aslan <nissanfordo03@gmail.com>
2024-08-09 13:18:17 +0500

    test

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,8 +1,6 @@
 import {Route, Routes} from 'react-router-dom'
 import Layout from "./Layout.jsx";
 import Main from "./Main.jsx";
-import SignIn from "./pages/SignIn.jsx";
-import SignUp from "./pages/SignUp.jsx";
 import {useEffect} from "react";
 
 const AppRouter = () => {
@@ -13,8 +11,6 @@ const AppRouter = () => {
     <Routes>
       <Route path={'/'} element={<Layout/>}>
         <Route index element={<Main/>}/>
-        <Route path={'login'} element={<SignIn/>}/>
-        <Route path={'register'} element={<SignUp/>}/>
       </Route>
     </Routes>
   )
